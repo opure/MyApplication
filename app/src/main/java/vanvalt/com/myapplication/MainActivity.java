e6b99fbe7a0456afd7790bb663643744dc1ef1c8
h.cao <h.cao@vanvalt.com>
2015-12-05 16:37:44 +0800

    ActionBar

--- app/src/main/java/vanvalt/com/myapplication/MainActivity.java
+++ app/src/main/java/vanvalt/com/myapplication/MainActivity.java
@@ -1,18 +1,24 @@
 package vanvalt.com.myapplication;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  public static final String EXTRA_MESSAGE = "com.vanvalt.mainActivity";
     private Button  button_dial;
+    private Button  button_send;
 
 
     @Override
@@ -22,7 +28,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Button button = (Button)findViewById(R.id.button_1);
         Button button1 = (Button)findViewById(R.id.button_2);
         button_dial = (Button)findViewById(R.id.button_dial);
-
+        button_send = (Button)findViewById(R.id.send);
+        button_send.setOnClickListener(this);
         button_dial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,29 +45,31 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 Intent intent = new Intent(MainActivity.this,SecondActitu.class);
              /*   intent.putExtra("extra_data",data);
                 startActivity(intent);*/
-                startActivityForResult(intent,1);
+                startActivityForResult(intent, 1);
             }
         });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Intent  intent = new Intent(MainActivity.this,SecondActitu.class);
+                // Intent  intent = new Intent(MainActivity.this,SecondActitu.class);
          /*       Intent intent = new Intent("com.vanvalt.yinshi");
                 intent.addCategory("com.vanvalt.mycategory");*/
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("http://www.baidu.com"));
-               // intent.setData(Uri.parse("tel:15514397873"));*/
+                // intent.setData(Uri.parse("tel:15514397873"));*/
                 startActivity(intent);
             }
         });
+         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.action_bar, menu);
         return true;
     }
 
@@ -95,12 +104,44 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-       /* switch (v.getId()) {
-            case R.id.button_1:
+       switch (v.getId()) {
+            case R.id.send:
+                Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+                EditText editText = (EditText) findViewById(R.id.edit_message);
+                String message = editText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
                 break;
             default:
                 break;
-        }*/
+        }
 
+    }
+     public void setOverIconVisable(Menu menu) {
+        try {
+            Class clazz = Class.forName("com.android.internal.view.menu.MenuBuilder");
+            Field field = clazz.getDeclaredField("mOptionalIconsVisible");
+            if (field != null) {
+                field.setAccessible(true);
+                field.set(menu,true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu !=null) {
+            setOverIconVisable(menu);
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        android.os.Debug.stopMethodTracing();
     }
 }
